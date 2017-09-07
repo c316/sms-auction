1e4040955bb64bbc19d1d89f0f908de36fff5063
JoshJoe <josh@joshjoe.com>
2017-09-07 17:05:27 -0500

    Added / route for testing purposes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(session({secret: sessionSecret}));
 
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end('Hello');
+});
 app.post('/sms', (req, res) => {
   let smsCount = req.session.counter || 0;
 
